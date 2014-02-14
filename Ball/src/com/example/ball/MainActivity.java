a8cedfbb9ee5ff1049e3d04efd955d7f24e75646
sukhifive <singhsac0@gmail.com>
2014-02-13 16:39:55 -0800

    q

--- Ball/src/com/example/ball/MainActivity.java
+++ Ball/src/com/example/ball/MainActivity.java
@@ -1,20 +1,25 @@
 package com.example.ball;
 
+import android.R.color;
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FrameLayout layout  = (FrameLayout) this.findViewById(R.layout.activity_main);
+        
+       // setContentView(R.layout.activity_main);
+//        DrawLine drawLine = new DrawLine(this);
+//        drawLine.setBackgroundColor(color.transparent);
+//        layout.addView(drawLine);
+    //    setContentView(drawLine);
         setContentView(R.layout.activity_main);
-        DrawLine drawLine = new DrawLine(this);
-        drawLine.setBackgroundColor(Color.TRANSPARENT);
-        setContentView(drawLine);
-      //  setContentView(R.layout.activity_main);
         
     }
 
